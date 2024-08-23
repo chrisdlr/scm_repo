98964d4cea934017f4fe3c3ae0de6545ba7293a4
Kleiver Perez <kleiver.perez.dev@gmail.com>
2024-08-22 23:09:04 -0400

    [IMP] - new release
    - Adding dashboard views.

---
+++ students_contract_management/static/src/components/chart_renderer/ChartRenderer.js
@@ -0,0 +1,134 @@
+/** @odoo-module **/
+import {loadJS} from "@web/core/assets";
+import {useService} from "@web/core/utils/hooks";
+
+const {Component, onWillStart, useRef, onMounted, useEffect, onWillUnmount} = owl;
+
+export class ChartRenderer extends Component {
+    setup() {
+        this.chartRef = useRef("chart");
+        this.actionService = useService("action");
+
+        onWillStart(async () => {
+            await loadJS("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js");
+            await loadJS("https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2");
+        });
+
+        useEffect(() => {
+            this.renderChart();
+        }, () => [this.props.config]);
+
+        onMounted(() => {
+            this.renderChart();
+        });
+
+        onWillUnmount(() => {
+            if (this.chart) {
+                this.chart.destroy();
+            }
+        });
+    }
+
+    renderChart() {
+        const oldChartJS = document.querySelector("script[src='/web/static/lib/Chart/Chart.js']");
+        if (oldChartJS) {
+            return;
+        }
+
+        if (this.chart) {
+            this.chart.destroy();
+        }
+        Chart.register(ChartDataLabels);
+        this.chart = new Chart(
+            this.chartRef.el,
+            {
+                type: this.props.type,
+                data: this.props.config.data,
+                options: {
+                    onClick: (e) => {
+                        const active = e.chart.getActiveElements();
+                        if (active) {
+                            const label = e.chart.data.labels[active[0].index];
+                            const dataset = e.chart.data.datasets[active[0].datasetIndex].label;
+
+                            const {labelField, domain} = this.props.config;
+                            let newDomain = domain ? domain : [];
+
+                            if (labelField) {
+                                if (labelField.includes('date')) {
+                                    const timeStamp = Date.parse(label);
+                                    const selectedMonth = moment(timeStamp);
+                                    const startMonth = selectedMonth.format('YYYY-MM-DD');
+                                    const endMonth = selectedMonth.endOf('month').format('YYYY-MM-DD');
+                                    newDomain.push(['date', '>=', startMonth], ['date', '<=', endMonth]);
+                                } else {
+                                    newDomain.push([labelField, '=', label]);
+                                }
+                            }
+
+                            if (dataset === 'Quotations') {
+                                newDomain.forEach((item) => {
+                                    if (item[0] === 'state') {
+                                        item[2] = ['draft', 'sent'];
+                                    }
+                                });
+                            }
+
+                            if (dataset === 'Orders') {
+                                newDomain.forEach((item) => {
+                                    if (item[0] === 'state') {
+                                        item[2] = ['sale', 'done'];
+                                    }
+                                });
+                            }
+
+                            this.actionService.doAction({
+                                type: 'ir.actions.act_window',
+                                name: this.props.title,
+                                res_model: "sale.report",
+                                domain: newDomain,
+                                views: [
+                                    [false, 'list'], [false, 'form']
+                                ],
+                            })
+                        }
+                    },
+                    responsive: true,
+                    plugins: {
+
+                        datalabels: {
+                            formatter: (value, categories) => {
+
+                                let sum = 0;
+                                let dataArr = categories.chart.data.datasets[0].data;
+                                dataArr.map(data => {
+                                    sum += data;
+                                });
+                                return (value * 100 / sum).toFixed(2) + "%";
+
+
+                            },
+                            color: '#ffffff',
+                            font: {
+                                weight: 'bold',
+                                size: 13,
+                            },
+                        },
+
+                        legend: {
+                            position: 'bottom',
+                        },
+                        title: {
+                            display: true,
+                            text: this.props.title,
+                            position: 'bottom',
+                        }
+                    },
+                    scales: 'scales' in this.props.config ? this.props.config.scales : {},
+                }
+            }
+        );
+    }
+}
+
+ChartRenderer.template = "chartRenderer";
